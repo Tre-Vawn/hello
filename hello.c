65370fe12ce82b9aeadddab6c10e089bf7603367
Tre-Vawn <trainey3@huskers.unl.edu>
2021-08-24 20:51:26 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Tre-Vawn Rainey
+ *         trainey3@huskers.unl.edu
+ * Date: 2021/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Tre-Vawn Rainey!\n");
+  printf("Computer Science\n");
+
+  return 0;
+}
